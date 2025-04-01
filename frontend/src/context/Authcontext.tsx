7165b41a6e63bbfb43c48f886a51daed97dadebf
Luis Saavedra <luisdavidsag@gmail.com>
2025-03-31 22:57:12 -0500

    Validate login status

--- frontend/src/context/Authcontext.tsx
+++ frontend/src/context/Authcontext.tsx
@@ -1,4 +1,5 @@
 import { useReducer, createContext, useEffect } from "react";
+import { getToken } from "../utils/utils";
 
 const initialState = {
   username: JSON.parse(localStorage.getItem("username")) || null
@@ -28,6 +29,34 @@ export const AuthProvider = ({ children }) => {
       localStorage.removeItem("username")
     }
   }, [state.username])
+
+  useEffect(() => {
+    const fetching = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/tasks/", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Token ${getToken()}`
+          }
+        })
+
+        if (!response.ok) {
+          dispatch({ type: "LOGOUT" })
+          localStorage.removeItem("username")
+
+        }
+      }
+      catch (error) {
+        dispatch({ type: "LOGOUT" })
+        localStorage.removeItem("username")
+      }
+    }
+    fetching()
+    const interval = setInterval(fetching, 300000)
+    return () => clearInterval(interval)
+
+  }, [])
 
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
