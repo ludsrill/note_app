a369fd8d056f33e6f094060b6b312143f28d8c9b
Luis Saavedra <luisdavidsag@gmail.com>
2025-04-19 23:07:32 -0500

    Add types for NavBar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,54 +1,56 @@
-import { useContext } from 'react'
+import { ReactElement, useContext } from 'react'
 import { NavLink as Link, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../context/Authcontext'
 import { LuLogOut } from 'react-icons/lu'
 
-const NavBar = () => {
+const NavBar = (): ReactElement | null => {
   const { username, dispatch } = useContext(AuthContext)
   const path = window.location.href.split('/')
   const currentPath = path.pop()
   const isLogin = currentPath === 'login'
   const isList = currentPath === 'list'
   const navigate = useNavigate()
-  const handleLogout = () => {
+  const handleLogout = (): void => {
     dispatch({ type: 'LOGOUT' })
   }
-  const handleLogin = () => {
-    navigate('/login')
+  const handleLogin = async (): Promise<void> => {
+    await navigate('/login')
   }
 
   return (
-    !isLogin
-      ? (
+    isLogin
+      ? null
+      : (
         <div className='flex items-center justify-between py-4 bg-sky-600 shadow-md w-full'>
 
-          {!isList
-            ? <nav className='flex space-x-4 ml-8'>
-              <Link to='/list' className='text-white hover:underline'>
-                Home
-              </Link>
-              </nav>
-            : null}
+          {isList
+            ? null
+            : (
+              <nav className='flex space-x-4 ml-8'>
+                <Link to='/list' className='text-white hover:underline'>
+                  Home
+                </Link>
+              </nav>)}
 
-          {username
+          {username !== null
             ? (
               <div className='flex items-center ml-auto mr-8'>
                 <div className='text-md text-white font-semibold mr-2'>
                   {username}
                 </div>
-                <button onClick={handleLogout}><LuLogOut color='white' /></button>
+                <button onClick={handleLogout}><LuLogOut color='white' />
+                </button>
               </div>
               )
-            : <div className='flex items-center ml-auto mr-8'>
-              <button className='flex items-center text-md text-white font-semibold mr-1' onClick={handleLogin}>
-                Login
-              </button>
-
-              </div>}
-
+            : (
+              <div className='flex items-center ml-auto mr-8'>
+                <button className='flex items-center text-md text-white font-semibold mr-1' onClick={() => { handleLogin().catch(() => { }) }}>
+                  Login
+                </button>
+              </div>
+              )}
         </div>
         )
-      : null
   )
 }
 
