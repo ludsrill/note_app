d4cc66f88a3975e2f5c21e99e3b0acd9de8bb4e2
Luis Saavedra <luisdavidsag@gmail.com>
2025-04-20 14:54:36 -0500

    Fix lint problems on login

--- frontend/src/pages/login/redirectIsAuthenticated.tsx
+++ frontend/src/pages/login/redirectIsAuthenticated.tsx
@@ -1,20 +1,20 @@
-import { useContext } from 'react'
+import { ReactElement, useContext } from 'react'
 import { AuthContext } from '../../context/Authcontext'
 import { Navigate } from 'react-router-dom'
 
-const RedirectLogin = ({ children }) => {
+const RedirectLogin = ({ children }): ReactElement => {
   const { username } = useContext(AuthContext)
 
-  if (username) {
+  if (username !== null) {
     return <Navigate to='/list' replace />
   }
   return children
 }
 
-const RedirectNoLogin = ({ children }) => {
+const RedirectNoLogin = ({ children }): ReactElement => {
   const { username } = useContext(AuthContext)
 
-  if (!username) {
+  if (username === null) {
     return <Navigate to='/' replace />
   }
   return children
