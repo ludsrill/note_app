5d623bd111aae6f557dbc9644390f43c8e87df15
Luis Saavedra <luisdavidsag@gmail.com>
2025-04-20 23:45:40 -0500

    imporve task model and serializer

--- backend/task/serializers.py
+++ backend/task/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import Task
+from django.contrib.auth.models import User
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    username = serializers.SlugRelatedField(
+        slug_field="username",
+        queryset=User.objects.all(),
+    )
 
     class Meta:
         model = Task
--- backend/task/views.py
+++ backend/task/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, mixins, status
+from rest_framework import generics, status
 from .models import Task
 from .serializers import TaskSerializer
 from rest_framework.response import Response
@@ -6,7 +6,6 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django.contrib.auth.models import User
-from .permission import isAdmin
 
 
 class StandardResultsSetPagination(PageNumberPagination):
@@ -15,7 +14,7 @@ class StandardResultsSetPagination(PageNumberPagination):
     max_page_size = 5
 
 
-class TaskListCreateAPIView(generics.ListCreateAPIView, generics.GenericAPIView):
+class TaskListCreateAPIView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     authentication_classes = [TokenAuthentication]
@@ -26,16 +25,7 @@ class TaskListCreateAPIView(generics.ListCreateAPIView, generics.GenericAPIView)
         return Task.objects.filter(username=self.request.user)
 
     def create(self, request, *args, **kwargs):
-        if isinstance(request.data["username"], str):
-            username = User.objects.get(username=request.data["username"])
-
-        serializer = TaskSerializer(
-            data={
-                "task": request.data["task"],
-                "title": request.data["title"],
-                "username": username.id,
-            }
-        )
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({"message": "Success"}, status.HTTP_200_OK)
@@ -56,22 +46,8 @@ class TaskRetriveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
 
     def update(self, request, *args, **kwargs):
-        if isinstance(request.data["username"], str):
-            username = User.objects.get(username=request.data["username"])
-
-        serializer = self.serializer_class(
-            data={
-                "id": request.data["id"],
-                "username": username.id,
-                "title": request.data["title"],
-                "task": request.data["task"],
-                "state": request.data["state"],
-                "priority": request.data["priority"],
-            }
-        )
-
-        task = Task.objects.filter(id=request.data["id"])
+        task = self.get_object()
+        serializer = self.get_serializer(task, data=request.data, partial=False)
         serializer.is_valid(raise_exception=True)
-        task.update(**serializer.validated_data)
-
+        serializer.save()
         return Response({"message": "Sucess"}, status.HTTP_200_OK)
